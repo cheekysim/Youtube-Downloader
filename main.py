9e1df8fbcd7a976f928f10ca783a19a981636c90
cheekysim <minecraftcheekysim@gmail.com>
2022-07-13 21:52:00 +0100

    made most the app

--- main.py
+++ main.py
@@ -1,6 +1,58 @@
 from pytube import YouTube
+from pytube.cli import on_progress
+import ffmpeg
+import shutil
+import os
+
 link = str(input("Video Link: "))
-yt = YouTube(link)
-stream = yt.streams.get_highest_resolution()
-stream.download()
-print("Download Successful")
+yt = YouTube(link, on_progress_callback=on_progress)
+ytname=yt.title
+streams = str(yt.streams)
+with open("temp.txt", "w") as f:
+    f.write(str(streams).replace(', ',',\n').replace('[', '\n').replace(']', '\n'))
+    f.close()
+streams = []
+[streams.append(line) for line in open('temp.txt') if 'Stream' in line]
+print("\n".join(streams))
+itag = input("Enter itag number of stream to download: ")
+stream = yt.streams.get_by_itag(itag)
+prog = []
+[prog.append(line) for line in open('temp.txt') if str(itag) in line]
+prog = ''.join(prog).split(' ')
+acodec = [i for i, s in enumerate(prog) if 'acodec' in s]
+vcodec = [i for i, s in enumerate(prog) if 'vcodec' in s]
+if vcodec:
+    if acodec:
+        print("\nDownloading Video")
+        stream.download(output_path="Downloads")
+    else:
+        print("WARNING\nVideo has no audio")
+        aitag = input("Enter itag number of stream or leave blank for no audio: ")
+        if aitag != "":
+            audio = yt.streams.get_by_itag(aitag)
+            print("Downloading Audio")
+            audio.download(output_path="temp/audio")
+            print("\nDownloading Video")
+            stream.download(output_path="temp/video")
+            print("\nMerging Audio and Video\n(This may take a while)")
+            for file in os.listdir("temp/video"):
+                if file.endswith(".mp4"): 
+                    video = ffmpeg.input(f"./temp/video/{ytname}.mp4")
+                elif file.endswith(".webm"):
+                    video = ffmpeg.input(f"./temp/video/{ytname}.webm")
+            for file in os.listdir("temp/audio"):
+                if file.endswith(".mp4"): 
+                    audio = ffmpeg.input(f"./temp/audio/{ytname}.mp4")
+                elif file.endswith(".webm"):
+                    audio = ffmpeg.input(f"./temp/audio/{ytname}.webm")
+            ffmpeg.concat(video, audio, v=1, a=1).output(f"./Downloads/{ytname}.mp4").run()
+            print("Done")
+        else:
+            print("Downloading Video")
+            stream.download(output_path="Downloads")
+elif acodec:
+    print("\nDownloading Audio")
+    stream.download(output_path="Downloads")
+os.remove('temp.txt')
+shutil.rmtree('temp')
+stream.on_complete(print("\nDownload Successful"))
